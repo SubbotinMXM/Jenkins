4b0d04938a5ea22112940f9e61c4a387264833d9
Maksim Subbotin <subbotin_ie@mail.ru>
2024-04-12 16:53:10 +0300

    init commit

---
+++ src/test/java/com/api/SimpleTests.java
@@ -0,0 +1,40 @@
+package com.api;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class SimpleTests {
+
+    @Test
+    void test1(){
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void test2(){
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void test3(){
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void test4(){
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void test5(){
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void test6(){
+        Assertions.assertTrue(false);
+    }
+    @Test
+    void test7(){
+        Assertions.assertTrue(false);
+    }
+    @Test
+    void test8(){
+        Assertions.assertTrue(false);
+    }
+}
