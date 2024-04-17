9eefca17b77dfa9f00c1d260fb765bb01ddb92a4
Maksim Subbotin <subbotin_ie@mail.ru>
2024-04-17 17:10:11 +0300

    Added tasks

--- src/test/java/com/api/SimpleTests.java
+++ src/test/java/com/api/SimpleTests.java
@@ -1,47 +1,57 @@
 package com.api;
 import org.junit.jupiter.api.*;
 
-@Tag("smoke")
+
 public class SimpleTests {
 
+    @Tag("smoke")
     @Test
     void test1(){
         Assertions.assertTrue(true);
     }
+    @Tag("smoke")
     @Test
     void test2(){
         Assertions.assertTrue(true);
     }
+
+    @Tag("web")
     @Test
     void test3(){
         Assertions.assertTrue(true);
     }
+    @Tag("web")
     @Test
     void test4(){
         Assertions.assertTrue(true);
     }
+    @Tag("web")
     @Test
     void test5(){
         Assertions.assertTrue(true);
     }
+    @Tag("prod")
     @Test
     void test6(){
         Assertions.assertTrue(false);
     }
+    @Tag("prod")
     @Test
     void test7(){
         Assertions.assertTrue(false);
     }
+    @Tag("prod")
     @Test
     void test8(){
         Assertions.assertTrue(false);
     }
-
+    @Tag("prod")
     @Test
     @Disabled
     void test9(){
         Assertions.assertTrue(false);
     }
+    @Tag("smoke")
     @Test
     @Disabled
     void test10(){
